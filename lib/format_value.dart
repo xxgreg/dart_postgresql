1b0c1e9ff89b08883b0aad280edb05a17ef8b2a3
Greg Lowe <greg@vis.net.nz>
2013-12-14 10:34:38 +1300

    Fix string substitution

--- lib/format_value.dart
+++ lib/format_value.dart
@@ -1,23 +1,24 @@
 part of postgresql;
 
+const int _apos = 39;
 const int _return = 13;
 const int _newline = 10;
 const int _backslash = 92;
 
 //TODO handle int modifiers
 dynamic _formatValue(value, String type) {
-  
+
   err(rt, t) => new Exception('Invalid runtime type and type modifier combination ($rt to $t).');
-  
+
   if (value == null)
     return 'null';
-    
+
   if (value is bool)
     return value.toString();
-  
+
   if (type != null)
     type = type.toLowerCase();
-  
+
   if (value is num) {
     if (type == null || type == 'number')
       return value.toString(); //TODO test that corner cases of dart.toString() match postgresql number types.
@@ -26,26 +27,26 @@ dynamic _formatValue(value, String type) {
     else
       throw err('num', type);
   }
-  
+
   if (value is String) {
     if (type == null || type == 'string')
       return _formatString(value);
     else
       throw err('String', type);
   }
-  
+
   if (value is DateTime) {
     //TODO check types.
     return _formatDateTime(value, type);
   }
-  
+
   //if (value is List<int>)
   // return _formatBinary(value, type);
-  
+
   throw new Exception('Unsupported runtime type as query parameter.');
 }
 
-final _escapeRegExp = new RegExp(r'[\r\n\\]');
+final _escapeRegExp = new RegExp(r"['\r\n\\]");
 
 //TODO test if this works without escaping unicode characters.
 // Uses an string constant E''.
@@ -53,35 +54,37 @@ final _escapeRegExp = new RegExp(r'[\r\n\\]');
 _formatString(String s) {
   if (s == null)
     return ' null ';
-  
+
   var escaped = s.replaceAllMapped(_escapeRegExp, (m) {
     switch (s.codeUnitAt(m.start)) {
+      case _apos: return r"\'";
       case _return: return r'\r';
       case _newline: return r'\n';
       case _backslash: return r'\\';
       default: assert(false);
     }
   });
+
   return " E'$escaped' ";
 }
 
 _formatDateTime(DateTime datetime, String type) {
-  
+
   if (datetime == null)
     return 'null';
-  
+
   String escaped;
   var t = (type == null) ? 'timestamp' : type.toLowerCase();
-  
+
   if (t != 'date' && t != 'timestamp' && t != 'timestamptz') {
     throw new Exception('Unexpected type: $type.'); //TODO exception type
   }
-  
+
   pad(i) {
     var s = i.toString();
     return s.length == 1 ? '0$s' : s;
   }
-  
+
   //2004-10-19 10:23:54.4455+02
   var sb = new StringBuffer()
     ..write(datetime.year)
@@ -89,7 +92,7 @@ _formatDateTime(DateTime datetime, String type) {
     ..write(pad(datetime.month))
     ..write('-')
     ..write(pad(datetime.day));
-  
+
   if (t == 'timestamp' || t == 'timestamptz') {
     sb..write(' ')
       ..write(pad(datetime.hour))
@@ -99,12 +102,12 @@ _formatDateTime(DateTime datetime, String type) {
       ..write(pad(datetime.second));
     //TODO Add fractional second datetime.millisecond;
   }
-      
+
   if (t == 'timestamptz') {
     // Add timezone offset.
     throw new Exception('Not implemented'); //TODO
   }
-  
+
   return "'${sb.toString()}'";
 }
 
--- test/substitute_test.dart
+++ test/substitute_test.dart
@@ -7,94 +7,104 @@ import 'package:postgresql/postgresql.dart';
 import 'package:yaml/yaml.dart';
 
 Settings loadSettings(){
-  var map = loadYaml(new File('test/test_config.yaml').readAsStringSync()); 
+  var map = loadYaml(new File('test/test_config.yaml').readAsStringSync());
   return new Settings.fromMap(map);
 }
 
 main() {
-  
+
   group('Substitute by id', () {
     test('Substitute 1', () {
       var result = substitute('@id', {'id': 20});
       expect(result, equals('20'));
     });
-    
-    test('Substitute 2', () {  
+
+    test('Substitute 2', () {
       var result = substitute('@id ', {'id': 20});
       expect(result, equals('20 '));
     });
-    
-    test('Substitute 3', () {    
+
+    test('Substitute 3', () {
       var result = substitute(' @id ', {'id': 20});
       expect(result, equals(' 20 '));
     });
-    
-    test('Substitute 4', () {    
+
+    test('Substitute 4', () {
       var result = substitute('@id@bob', {'id': 20, 'bob': 13});
       expect(result, equals('2013'));
     });
-    
-    test('Substitute 5', () {    
+
+    test('Substitute 5', () {
       var result = substitute('..@id..', {'id': 20});
       expect(result, equals('..20..'));
     });
-    
-    test('Substitute 6', () {    
+
+    test('Substitute 6', () {
       var result = substitute('...@id...', {'id': 20});
       expect(result, equals('...20...'));
     });
-    
-    test('Substitute 7', () {    
+
+    test('Substitute 7', () {
       var result = substitute('...@id.@bob...', {'id': 20, 'bob': 13});
       expect(result, equals('...20.13...'));
     });
-    
-    test('Substitute 8', () {    
+
+    test('Substitute 8', () {
       var result = substitute('...@id@bob', {'id': 20, 'bob': 13});
       expect(result, equals('...2013'));
     });
-    
-    test('Substitute 9', () { 
+
+    test('Substitute 9', () {
       var result = substitute('@id@bob...', {'id': 20, 'bob': 13});
       expect(result, equals('2013...'));
     });
-    
-    test('Substitute 10', () { 
+
+    test('Substitute 10', () {
       var result = substitute('@id:string', {'id': 20, 'bob': 13});
       expect(result, equals("'20'"));
     });
-    
-    test('Substitute 11', () { 
+
+    test('Substitute 11', () {
       var result = substitute('@blah_blah', {'blah_blah': 20});
       expect(result, equals("20"));
     });
 
-    test('Substitute 12', () { 
+    test('Substitute 12', () {
       var result = substitute('@_blah_blah', {'_blah_blah': 20});
       expect(result, equals("20"));
     });
 
+//    test('Substitute 13', () {
+//      var result = substitute('@apos', {'apos': "'"});
+//      //expect(result, equals("E'''"));
+//      //print('oi');
+//      print(result);
+//    });
   });
-  
-  
+
+
   test('Format value', () {
     expect(formatValue('bob', null), equals(" E'bob' "));
     expect(formatValue('bo\nb', null), equals(r" E'bo\nb' "));
     expect(formatValue('bo\rb', null), equals(r" E'bo\rb' "));
     expect(formatValue(r'bo\b', null), equals(r" E'bo\\b' "));
+
+    expect(formatValue(r"'", null), equals(r" E'\'' "));
+    expect(formatValue(r" '' ", null), equals(r" E' \'\' ' "));
+    expect(formatValue(r"\''", null), equals(r" E'\\\'\'' "));
   });
-  
-  
+
+
   group('Query', () {
-    
+
     Connection conn;
-    
+
     setUp(() => connect(loadSettings().toUri()).then((c) => conn = c));
-    
+
     tearDown(() {
       if (conn != null) conn.close();
     });
-  
+
     test('Substitution', () {
       conn.query(
           'select @num, @num:string, @num:number, '
@@ -102,7 +112,7 @@ main() {
           '@string, '
           '@datetime, @datetime:date, @datetime:timestamp, '
           '@boolean, @boolean_false, @boolean_null',
-          { 'num': 1.2, 
+          { 'num': 1.2,
             'int': 3,
             'string': 'bob\njim',
             'datetime': new DateTime(2013, 1, 1),
@@ -112,7 +122,7 @@ main() {
           }).toList()
             .then(expectAsync1((rows) {}));
     });
-    
+
   });
-  
+
 }
